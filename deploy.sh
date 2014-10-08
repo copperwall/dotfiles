56adae69581bf7d9f3f090342c006b5795e67ef3
Chris Opperwall <chriso@ifixit.com>
2014-10-07 23:25:00 -0400

    Update deploy.sh to include tmux.conf
    Also includes a conditional to exclude desktop-specific configuration
    files.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-# I realize I could have just used cp -v instead of the echo's
-# Perhaps I'll change it in the future
+echo "Deploying configuration..."
 
-echo "Deploying..."
-
-cp -v .Xdefaults $HOME
+if [ "$1" != "server" ]; then
+	cp -v .xdefaults $home
+fi
 
 cp -v .zshrc $HOME
-
 cp -v vim/.vimrc $HOME
-
 cp -r vim/.vim $HOME
-
 cp -v .gitconfig $HOME
+cp -v .tmux.conf $HOME
 
 echo "..done!"
